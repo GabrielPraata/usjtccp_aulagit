720242b3ed1c8d97026eb425a586014655eda7ce
Gabriel Prata <ghprata7245@gmail.com>
2020-09-08 17:51:27 -0300

    Comit inicial, Classe Empregado foi criada

---
+++ Empregado.java
@@ -0,0 +1,18 @@
+public class Empregado{
+    private string nome;
+    private int idade;
+    public string getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+
+}
